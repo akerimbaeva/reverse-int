5cce4d57478ecab3faf29cd2a4d20c09d8c851db
akerimbaeva <akerimbaeva@gmail.com>
2022-02-22 14:22:12 +0600

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n)
+      let str = n.toString()
+      let num = '';  
+        for(let i = str.length - 1; i>=0; i--) {
+            num = num + str[i]
+        }
+        num = Number(num)
+        return num
+    }
+    
